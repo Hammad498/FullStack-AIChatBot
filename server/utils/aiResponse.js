230580173db983830ac18f968baeeed0545b4f90
Hammad498 <hammadabrar498@gmail.com>
2025-07-21 05:25:55 -0700

    some cache

--- server/utils/aiResponse.js
+++ server/utils/aiResponse.js
@@ -1,10 +1,26 @@
 import dotenv from "dotenv";
 import axios from "axios";
+import redisClient from "../config/redisClient.js";
+import crypto from "crypto";
 dotenv.config();
 
 
 
 const AIResponse = async (message) => {
+
+    const hash=crypto.createHash('sha256').update(message).digest('hex');
+    const cacheKey=`ai:${hash}`;
+    const cached=await redisClient.get(cacheKey);
+    if (cached) {
+    console.log(" Serving from cache");
+    return cached;
+     } else {
+    console.log(" Cache miss, generating AI response");
+   }
+
+
+
+
     try {
         const response = await axios.post(
             process.env.AIResponse_URL,
@@ -26,7 +42,15 @@ const AIResponse = async (message) => {
         console.log("API Key:", process.env.OPENROUTER_API_KEY ? " Loaded" : " Missing");
 
 
-        return response.data?.choices?.[0]?.message?.content || "NO response";
+        const aiReply=response.data?.choices?.[0]?.message?.content || "NO response";
+
+
+        await redisClient.setEx(cacheKey,3600,aiReply);
+
+        return aiReply;
+
+
+
     } catch (error) {
         console.error("Error fetching AI response:", error.response?.data || error.message);
         throw new Error("Failed to fetch AI response");
