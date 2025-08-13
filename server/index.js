afbd8272b68515bc74523b1771a476cc82aed391
Muhammad Hammad Abrar <hammadabrar498@gmail.com>
2025-08-13 13:29:16 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+//////////////////////////////////////////////////
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
